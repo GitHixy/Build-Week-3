b6b1a892381ae7bf489ea75d263f0f6f81429ed9
ang <angelo03giuliano@gmail.com>
2024-01-30 20:06:51 +0100

    up fetch

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
+const url = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 const cardCont = document.getElementById("card-container");
+const search = document.getElementById('search');
+const input = document.getElementById('input');
+
+search.addEventListener('click', () => {
+  input.classList.toggle('d-none')
+})
 
 const getData = async () => {
-  const response = await fetch(url);
+  const response = await fetch(url + "queen");
   const data = await response.json();
 
   console.log(data.data);
@@ -37,8 +43,48 @@ const getData = async () => {
             console.log(artist.closest('p').attributes.id.value);
         })
     }
-
   });
+
 };
 
 getData();
+
+const fetchSearch = async () => {
+  cardCont.innerHTML = ""
+  const response = await fetch(url + input.value)
+  const data = await response.json()
+
+  data.data.map((found) => {
+    cardCont.innerHTML += `
+        <div class="card d-flex align-items-start col-lg-2">
+        <img
+            id=${found.album.id}
+          src=${found.album.cover_big}
+          class="album-img"
+          alt=""
+        />
+        <div class="card-body pt-0">
+          <h5 class="card-title text-white">${found.album.title}</h5>
+          <p id=${found.artist.id} class="card-text">
+            ${found.artist.name}
+          </p>
+        </div>
+      </div>`;})
+
+      const albumImg = document.getElementsByClassName("album-img")
+      const artistText = document.getElementsByClassName("card-text")
+  
+      for (const album of albumImg) {
+          album.addEventListener('click', () => {
+              console.log(album.closest('img').attributes.id.value);
+          })
+      }
+  
+      for (const artist of artistText) {
+          artist.addEventListener('click', () => {
+              console.log(artist.closest('p').attributes.id.value);
+          })
+      }
+    };
+
+input.addEventListener('change', fetchSearch)
