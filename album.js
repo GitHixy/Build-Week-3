84929e22bec24fe54cc7581bd1ad1723b7ea51e8
ang <angelo03giuliano@gmail.com>
2024-01-31 16:48:06 +0100

    up js + css

---
+++ album.js
@@ -0,0 +1,48 @@
+const url = "https://striveschool-api.herokuapp.com/api/deezer";
+const params = new URLSearchParams(document.location.search) 
+const id = params.get("id") 
+
+const getAlbum = async () => {
+    const response = await fetch(url + `/album/${id}`);
+    const data = await response.json();
+  
+    console.log(data.tracks.data);
+    main.innerHTML += `
+  <div class="artist-info">
+            <div class="artistImg">
+                <img src=${data.cover_xl}
+                    alt="">
+                </div>
+                <h1 class="text-white">${data.title}</h1>
+                <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Duration</th>
+              </tr>
+            </thead>
+            <tbody id="tbody">
+            </tbody>
+          </table>
+        </div>
+  `;
+
+  const traclkistOl = document.getElementById('tbody')
+  let songNumber = 1
+  for (const track of data.tracks.data) {
+    let trackDuration = ((track.duration/60).toFixed(2))
+    console.log(trackDuration);
+    traclkistOl.innerHTML += `
+    <tr>
+                <th scope="row">${songNumber}</th>
+                <td>${track.title}</td>
+                <td>${trackDuration}</td>
+              </tr>
+    `
+    songNumber += 1
+  }
+
+}
+  
+  getAlbum();
--- index.js
+++ index.js
@@ -39,12 +39,14 @@ const getData = async () => {
     for (const album of albumImg) {
       album.addEventListener("click", () => {
         console.log(album.closest("img").attributes.id.value);
+        window.location.assign('./album.html?id=' + album.closest("img").attributes.id.value)
       });
     }
 
     for (const artist of artistText) {
       artist.addEventListener("click", () => {
         console.log(artist.closest("p").attributes.id.value);
+        window.location.assign('./artist.html?id=' + artist.closest("p").attributes.id.value)
       });
     }
   });
@@ -53,45 +55,50 @@ const getData = async () => {
 getData();
 
 const fetchSearch = async () => {
-  cardCont.innerHTML = "";
-  const response = await fetch(url + input.value);
-  const data = await response.json();
-
-  data.data.map((found) => {
-    if (!addedId[found.album.id]) {
-      cardCont.innerHTML += `
-      <div class="card d-flex align-items-start col-lg-2">
-      <img
-          id=${found.album.id}
-        src=${found.album.cover_big}
-        class="album-img"
-        alt=""
-      />
-      <div class="card-body pt-0">
-        <h5 class="card-title text-white">${found.album.title}</h5>
-        <p id=${found.artist.id} class="card-text">
-          ${found.artist.name}
-        </p>
-      </div>
-    </div>`;
-      addedId[found.album.id] = true;
+  if (input.value !== "") {
+    cardCont.innerHTML = "";
+    const response = await fetch(url + input.value);
+    const data = await response.json();
+  
+    data.data.map((found) => {
+      if (!addedId[found.album.id]) {
+        cardCont.innerHTML += `
+        <div class="card d-flex align-items-start col-lg-2">
+        <img
+            id=${found.album.id}
+          src=${found.album.cover_big}
+          class="album-img"
+          alt=""
+        />
+        <div class="card-body pt-0">
+          <h5 class="card-title text-white">${found.album.title}</h5>
+          <p id=${found.artist.id} class="card-text">
+            ${found.artist.name}
+          </p>
+        </div>
+      </div>`;
+        addedId[found.album.id] = true;
+      }
+    });
+  
+    const albumImg = document.getElementsByClassName("album-img");
+    const artistText = document.getElementsByClassName("card-text");
+  
+    for (const album of albumImg) {
+      album.addEventListener("click", () => {
+        console.log(album.closest("img").attributes.id.value);
+        window.location.assign('./album.html?id=' + album.closest("img").attributes.id.value)
+      });
     }
-  });
-
-  const albumImg = document.getElementsByClassName("album-img");
-  const artistText = document.getElementsByClassName("card-text");
-
-  for (const album of albumImg) {
-    album.addEventListener("click", () => {
-      console.log(album.closest("img").attributes.id.value);
-    });
+  
+    for (const artist of artistText) {
+      artist.addEventListener("click", () => {
+        console.log(artist.closest("p").attributes.id.value);
+        window.location.assign('./artist.html?id=' + artist.closest("p").attributes.id.value)
+      });
+    }
+  };
+  
   }
-
-  for (const artist of artistText) {
-    artist.addEventListener("click", () => {
-      console.log(artist.closest("p").attributes.id.value);
-    });
-  }
-};
-
+  
 input.addEventListener("change", fetchSearch);
